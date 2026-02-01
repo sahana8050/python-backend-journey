a95391c6b42af09a499c90708e94417afaa25d27
Sahana <sahanar8050@gmail.com>
2026-02-01 08:04:20 +0530

    learning for loop

---
+++ for.py
@@ -0,0 +1,14 @@
+# printing 0-4 integers numbers
+for i in range(5):
+    print(i)
+
+
+    ## printig 1-5 integers numbers
+for i in range(1,6):
+    print(i)
+
+
+#printing even numbers
+for i in range(2,11,  2):
+    print(i)
+    
