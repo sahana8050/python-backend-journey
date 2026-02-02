c0b2e1f7c83905fdd188d6aa570e8158931d64b3
Sahana <sahanar8050@gmail.com>
2026-02-02 18:37:07 +0530

    for loop

---
+++ for.py
@@ -0,0 +1,16 @@
+# printing 0-4 integers numbers
+print("0 to 4:")
+for i in range(5):
+    print(i)
+
+
+    ## printig 1-5 integers numbers
+print("1 to 5:")
+for i in range(1,6):
+    print(i)
+
+
+#printing even numbers
+print("Event numbers:")
+for i in range(2,11,  2):
+    print(i)
