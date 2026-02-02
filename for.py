5bf675501183d3a337ed4882aa0e9c8334f3cee1
Sahana <sahanar8050@gmail.com>
2026-02-02 18:55:42 +0530

    for loop

--- for.py
+++ for.py
@@ -14,3 +14,9 @@ for i in range(1,6):
 print("Event numbers:")
 for i in range(2,11,  2):
     print(i)
+
+ 
+ ## squaring  numbers
+    print("squaring numbers:")
+for i in range(5):
+    print(i*i)
