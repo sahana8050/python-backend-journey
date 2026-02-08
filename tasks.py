20a315ffb30ee61623abe77fa9cda89f62808527
Sahana <sahanar8050@gmail.com>
2026-02-08 20:46:09 +0530

    for loop

--- tasks.py
+++ tasks.py
@@ -9,4 +9,20 @@ for i in range(1,16):
 
 print("odd number from 1 to 15:")
 for i in range(1,16,2):                   ## we can print odd and even numbers by taking steps (start,stop,step)
-    print(i)                              #in step it consider that value to take step
+    print(i)  #in step it consider that value to take step
+
+print("0-5 revers order:")
+for i in range(5, 0, -1):
+    print(i)
+    
+print("multiples of 3:")
+for i in range(3,31):
+    if i%3==0:
+        print(i)
+
+print("even or odd:")
+for i in range(1,11):
+    if i%2==0:
+        print("even")
+    else:
+        print("odd")
