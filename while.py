b4cf31454da92a93c5c59f2d81ffd55b83ad0803
Sahana <sahanar8050@gmail.com>
2026-02-11 19:17:18 +0530

    while

--- while.py
+++ while.py
@@ -1,4 +1,31 @@
+print("1 to 5 numbers:")
 i=1
 while i<=5:
     print(i)
     i=i+1
+
+print("1 to 10 numbers:")
+i=1
+while i<=10:
+    print(i)
+    i=i+1
+
+print("even numbers:")
+i=2
+while i<20:
+    print(i)
+    i=i+2
+
+print("odd numbers:")
+i=1
+while i<=20:
+    print(i)
+    i=i+2
+
+print("counting numberes:")
+i=1
+count=0
+while i<=50:
+    count=count+1
+    i=i+1
+print("Total numbers are:",count)
