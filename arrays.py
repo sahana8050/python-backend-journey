ee993f0e45b75946d67fd17eb28a5b340119cb68
Sahana <sahanar8050@gmail.com>
2026-02-14 14:46:47 +0530

    arrays

--- arrays.py
+++ arrays.py
@@ -15,6 +15,7 @@ for x in arr:
 for x in arr:
     print(x, end=",") # \n is a new line
 
+
 print("\n maximum value:") #printing maximum value
 arr=[1,5,7,9,24]
 max_value=arr[0]
@@ -23,6 +24,7 @@ for x in arr:
         max_value=x
 print(max_value)
 
+
 print("\n minimum value")
 arr=[1,3,6,7,8]
 min_value=arr[0]
@@ -30,6 +32,26 @@ for x in arr:
     if x<=min_value:
      mini_value=x
 print(mini_value)
+
+
+print("second largest number:")
+arr=[5,1,9,3,97,8]
+largest=arr[0]
+second_largest=[-1]
+for x in arr:
+    if x>=largest:
+        second_largest=largest
+        largest=x
+    elif x>=second_largest and x!=largest:
+        second_largest=x
+print(second_largest)
+
+
+
+print(":")
+
+
+
 
 
 
