afc4e747bda9b67862c467673f4f52e8877255e1
Sahana <sahanar8050@gmail.com>
2026-02-21 16:56:09 +0530

    string.py

---
+++ str.py
@@ -0,0 +1,16 @@
+##in python strings are immutable means non changeble if we want to change they will allocated in another memory not replace current path
+#example for immutable str in python
+             #str="sahana"
+             #str[0]= "R"
+             #print(str)
+
+print("length of string is :")
+str="sahana R"
+str=len(str)
+print(str)
+
+
+
+
+
+
