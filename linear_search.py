08709a94e36edc88c94e892eb738f6214802fd84
Sahana <sahanar8050@gmail.com>
2026-03-10 18:10:05 +0530

    linear_search

---
+++ linear_search.py
@@ -0,0 +1,18 @@
+a=[10,30,20,60]
+key=60
+index=0
+while index<len(a):
+ 
+   if key==a[index]:
+      print("True")
+      break
+   index=index+1
+else:
+ print("False") 
+  
+
+   
+  
+  
+
+        
