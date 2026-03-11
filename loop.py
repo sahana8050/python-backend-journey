0f5776d56c2959b671af1679ed71d4ad3b9bb6e5
Sahana <sahanar8050@gmail.com>
2026-03-11 22:15:47 +0530

    loops

---
+++ loop.py
@@ -0,0 +1,7 @@
+print("1 to 10 numbers:")     # this function also called as polymorphisum :same function but behave different and same output
+for i in range(11):
+ print(i)
+
+print("1 to 10  numbers:")
+for s in range(1,11):
+ print(s)
