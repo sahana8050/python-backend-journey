da93af6653e68f9cfaec8a8b1fed4826637af986
Sahana <sahanar8050@gmail.com>
2026-03-12 21:13:56 +0530

    nested_loop

--- Nested_loop.py
+++ Nested_loop.py
@@ -1,6 +1,8 @@
-print("Multipliccation tabel")
-for tabel in range(1,11):
-    for s in range(1,11):
-     print(tabel*s)
+print("Multipliccation tabel") # in nested loop always outer loop decides table number and inner loop decides the multiples numn=bers from given counr example 1 to 10
+for n in range(1,11):  #outer loop
+    for s in range(1,11):#inner loop
+     print(n,"X", s, "=", n*s)
+     
+    print("------------------") # after every table blank line shows it will esay to understanding
    
 
